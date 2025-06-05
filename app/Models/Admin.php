5fa823c8a6b9616fe9a2427e44e088278cbdc141
JoshR95 <joshuarickards@hotmail.co.uk>
2025-06-05 11:32:52 +0100

    done admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+// provides auth features
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+// extending Authenticatable allows us to use log in, maintain sessions, password reset and remember me functions
+class Admin extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    
+    protected $hidden = [
+        'password',
+    ];
 }
