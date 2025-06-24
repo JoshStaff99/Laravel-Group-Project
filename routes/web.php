eebadaef50cbb7a12cedbc1fb54ee23d0b270a08
roff-dev <kieronoates@hotmail.co.uk>
2025-06-24 16:04:22 +0100

    Product Controller + middleware
    Completed the crud functionality for the product controller. created an admin middleware so that only the admin can access the product functionality and customers cannot
    updated the product and user migrations to include needed information like role and quote id
    added middleware routing to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('products', ProductController::class);
 });
